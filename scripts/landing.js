7b465c78a724503bc5f692852998caa1618ac2ce
Cord Wartman <cordwartman@gmail.com>
2016-10-27 18:56:44 -0400

    made a forEach function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+ var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1)    translateY(0)";
+        point.style.WebkitTranfrom = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
-    
-    var revealPoint = function(iPoint) {
-        points[iPoint].style.opacity = 1;
-        points[iPoint].style.transform = "scaleX(1) translateY(0)";
-        points[iPoint].style.msTransform = "scaleX(1)    translateY(0)";
-        points[iPoint].style.WebkitTranfrom = "scaleX(1) translateY(0)";
-    }
-    
-    for(var i=0; i<points.length; i++){
-        revealPoint(i);
-    }
+   forEach(points, revealPoint); 
 };
 
 window.onload = function() {

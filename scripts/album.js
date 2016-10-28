9015552c7d8c785c3ee359751a479c71aaf35ed5
Cord Wartman <cordwartman@gmail.com>
2016-10-28 15:08:12 -0400

    added a click through loop to rotate through albums

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,25 @@ var albumMarconi = {
     ]
 };
 
+var albumTurisas = {
+    title: 'Stand Up and Fight',
+    artist: 'Turisas',
+    label: 'Century Media',
+    year: '2011',
+    albumArtUrl: 'assets/images/album_covers/turisas.jpg',
+    songs: [
+        { title: 'The March of the Varangian Guard', duration: '3:51' },
+        { title: 'Take the Day!', duration: '5:26' },
+        { title: 'Hunting Pirates!', duration: '3:43'},
+        { title: 'Venetoi! - Prasinoi!', duration: '3:49' },
+        { title: 'Stand Up and Fight', duration: '5:27'},
+        { title: 'The Great Escape', duration: '4:51'},
+        { title: 'Fear the Fear', duration: '6:13'},
+        { title: 'End of an Empire', duration: '7:13'},
+        { title: 'The Bosphorus Freezes Over', duration: '5:37'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
        '<tr class="album-view-song-item">'
@@ -39,14 +58,14 @@ var createSongRow = function(songNumber, songName, songLength) {
  
      return template;
  };
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
         
 var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -61,4 +80,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function () {
     setCurrentAlbum(albumPicasso);
+
+    var albums = [albumPicasso, albumMarconi, albumTurisas];
+    var index = 1;
+    albumImage.addEventListener('click', function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+        index = 0;
+        }
+    });
 };
